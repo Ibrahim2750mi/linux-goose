85e008f25e033a2767dfb057c319bd3793ebf0ae
Ibrahim <cshore.2750mi@gmail.com>
2021-10-14 14:12:26 +0530

    added untracked files.

---
+++ build/CMakeFiles/goose.dir/DependInfo.cmake
@@ -0,0 +1,36 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/ibrahim/C++/linux-goose/linux-goose/src/Goose.cpp" "/home/ibrahim/C++/linux-goose/linux-goose/build/CMakeFiles/goose.dir/Goose.cpp.o"
+  "/home/ibrahim/C++/linux-goose/linux-goose/build/goose_autogen/mocs_compilation.cpp" "/home/ibrahim/C++/linux-goose/linux-goose/build/CMakeFiles/goose.dir/goose_autogen/mocs_compilation.cpp.o"
+  "/home/ibrahim/C++/linux-goose/linux-goose/src/main.cpp" "/home/ibrahim/C++/linux-goose/linux-goose/build/CMakeFiles/goose.dir/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "QT_CORE_LIB"
+  "QT_GUI_LIB"
+  "QT_NO_DEBUG"
+  "QT_WIDGETS_LIB"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "goose_autogen/include"
+  "/usr/include/x86_64-linux-gnu/qt5"
+  "/usr/include/x86_64-linux-gnu/qt5/QtWidgets"
+  "/usr/include/x86_64-linux-gnu/qt5/QtGui"
+  "/usr/include/x86_64-linux-gnu/qt5/QtCore"
+  "/usr/lib/x86_64-linux-gnu/qt5/mkspecs/linux-g++"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/goose_autogen.dir/cmake_clean.cmake
@@ -0,0 +1,9 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/goose_autogen"
+  "goose_autogen/mocs_compilation.cpp"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/goose_autogen.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
